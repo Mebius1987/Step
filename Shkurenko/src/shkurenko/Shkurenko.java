d130755dff800882df2d1a12075b3a7e2f4c581e
Andrey Shkurenko <mebius1987@gmail.com>
2015-12-06 09:18:50 +0200

    first commit

---
+++ Shkurenko/src/shkurenko/Shkurenko.java
@@ -0,0 +1,225 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shkurenko;
+
+/**
+ *
+ * @author Mebius
+ */
+public class Shkurenko {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    //              1
+        /*    System.out.println();
+        System.out.println("\t\"To be");
+        System.out.println("\t\t or not");
+        System.out.println("\t\t\t to be...\"");
+        System.out.println("\t\t\t\t/ Shakespeare/");
+    
+    */
+   //                 2
+    //System.out.println("I love Java!\n\tI love Java!\n\t\tI love Java!\n\t\t\tI love Java!\n\t\t\t\tI love Java!");
+    //                3
+    //System.out.println("'\\t', '\\n', '\\\\', '\\'', '\\\"'-");
+    //                  4
+    /*
+    System.out.println("package shkurenko;");
+    System.out.println("public class Shkurenko {");
+    System.out.println("\tpublic static void main(String[] args) {");
+    System.out.println("\t\tSystem.out.println(\"'\\\\t', '\\\\n', '\\\\\\\\', '\\\\'', '\\\\\\\"'-\");");
+    System.out.println("\t}");
+    System.out.println("}");
+    */
+    /*
+                      5
+    System.out.println("      декабрь");//месяц
+    System.out.println("пн вт ср чт пт сб вс");//день недели
+    System.out.println("    1  2  3  4  5  6");//неделя 1
+    System.out.println(" 7  8  9 10 11 12 13");//неделя 2
+    System.out.println("14 15 16 17 18 19 20");//неделя 3
+    System.out.println("21 22 23 24 25 26 27");//неделя 4
+    System.out.println("28 29 30 31");//неделя 5 
+    */
+    //                  6
+    /*
+        System.out.println("                            *****");
+        System.out.println("                        ****** *****     *****    ");
+        System.out.println("                    *****          ***** *   *");
+        System.out.println("                ******                *****  *");
+        System.out.println("            *****                        ******");
+        System.out.println("        ******                               *****");
+        System.out.println("    *****                                       *****");
+        System.out.println(" *******************************************************");
+        System.out.println("*** *                                               * ***");
+        System.out.println("    *                                               *");
+        System.out.println("    *       *********               *********       *");
+        System.out.println("    *       *   *   *               *       *       *");
+        System.out.println("    *       *********               *       *       *       **  **  **  **  **  **  **  **  **  **");
+        System.out.println("    *       *   *   *               *       *       *       **  **  **  **  **  **  **  **  **  **");
+        System.out.println("    *       *********               *       *       *     ******************************************");
+        System.out.println("    *                               *       *       *       **  **  **  **  **  **  **  **  **  **");
+        System.out.println("    *                               *       *       *       **  **  **  **  **  **  **  **  **  **");
+        System.out.println("    *                               *       *       *     ******************************************");
+        System.out.println("    *************************************************       **  **  **  **  **  **  **  **  **  **");
+    */
+    //                7 версия 1
+    /*
+        System.out.println("                                        Шкуренко Андрей Викторович");
+        System.out.println("                                                    *");
+        System.out.println("                                                    *");
+        System.out.println("                                                    *");
+        System.out.println("                                                   * *");
+        System.out.println("                                                  *   *");
+        System.out.println("        Шкуренко Людмила Петровна                               Шкуренко Виктор Сергеевич");
+        System.out.println("                       *                                                   *");
+        System.out.println("                       *                                                   *");
+        System.out.println("                       *                                                   *");
+        System.out.println("                      * *                                                 * *");
+        System.out.println("                     *   *                                               *    *");    
+        System.out.println("Бойченко Петр Николаевич Бойченко Феодосия Васильевна Шкуренко Сергей Лукич Шкуренко Лиля Андреевна");
+        */
+        /*
+        System.out.println("                                        Шкуренко Андрей Викторович");
+        System.out.println("                                                     *");
+        System.out.println("                                                     *");
+        System.out.println("                                                     *");
+        System.out.println("                                                    * *");
+        System.out.println("                                                   *   *");
+        System.out.println("                            Шкуренко Людмила Петровна   Шкуренко Виктор Сергеевич");
+        System.out.println("                                        *                               *");
+        System.out.println("                                        *                               *");
+        System.out.println("                                        *                               *");
+        System.out.println("                                       * *                             * *");
+        System.out.println("                                      *   *                           *   *");
+        System.out.println("                            Дедушка Петя  Бабушка Феодосия Дедушка Сергей Бабушка Лиля");
+        */
+        //                8
+        /*
+        System.out.println("Андрей");
+        System.out.println("        ****    **      **     **********     **********   ********        ***");
+        System.out.println("       ******   **      **     **********     **     **    ********    **       ***");
+        System.out.println("      **   **   **      **     **      **     **    **     **          **      ****");
+        System.out.println("     **    **   **      **     **      **     **    **     ********    **     ** **");
+        System.out.println("    *********   **********     **      **     *******      ********    **    **  **");
+        System.out.println("   **********   **********     **********     ******       **          **   **   **");
+        System.out.println("  **       **   **      **    ************    **           **          **  **    **");
+        System.out.println(" **        **   **      **    **        **    **           ********    ** **     **");
+        System.out.println("**         **   **      **   **          **   **           ********    ***       **");
+        */
+        /*
+        System.out.println("Шкуренко Андрей");
+        System.out.println("Дата рождения:	03 сен 1987");
+        System.out.println("Регион:	Одесса");
+        System.out.println("Телефон(ы):	093-7933334");
+        System.out.println("E-mail:	holdenmebius@rambler.ru");
+        System.out.println("Системный администратор ");
+        System.out.println("Цель");
+        System.out.println("_____________________________________________________________________________");
+        System.out.println("Цель");
+        System.out.println("Ит менеджер, системный по обслуживанию розничной , оптовой торговли либо ");
+        System.out.println("логистическую деятельность");
+        System.out.println("Торговля и логистика");
+        System.out.println("Создание и обслуживания рабочих точек (кассир , администратор,), обучение персонала ");
+        System.out.println("работы с специализированными программами обслуживания этих программ ");
+        System.out.println("В развивающийся использующие новые технологии ");
+        System.out.println("Опыт работы");
+        System.out.println("Инженер охранных систем ");
+        System.out.println("окт 2014 - настоящее время (1 год 3 мес)");
+        System.out.println("Олимп-юг (http://ru.olimp-ug.com.ua/)");
+        System.out.println("от 50 до 100 сотрудников  Охрана и Безопасность    ");
+        System.out.println("");
+        System.out.println("Инженер пожарной безопасности");
+        System.out.println("сен 2013 - окт 2014 (1 год 2 мес)");
+        System.out.println("Укрохрансервис ");
+        System.out.println("от 20 до 50 сотрудников  Охрана и Безопасность    ");
+        System.out.println("");
+        System.out.println("Инженер охранных систем");
+        System.out.println("мар 2012 - сен 2013 (1 год 7 мес)");
+        System.out.println("Олимп-юг (http://ru.olimp-ug.com.ua/)");
+        System.out.println("от 20 до 50 сотрудников  Охрана и Безопасность    ");
+        System.out.println("");
+        System.out.println("Системный администратор");
+        System.out.println("ноя 2011 - фев 2012 (4 мес)");
+        System.out.println("ООО \"Арикол\" (http://aricol.com)");
+        System.out.println("от 100 до 250 сотрудников  Торговля оптовая / Дистрибуция / Импорт-экспорт    ");
+        System.out.println("Обслуживание принтеров Richon 2000, 8100, 8200");
+        System.out.println("Настройка и программирование Мини-АТС Panasonic KX-TD1232");
+        System.out.println("Работа с win 2008 AD GPO");
+        System.out.println("Закупка оборудование ИТ");
+        System.out.println("");
+        System.out.println("Инженер по применению компьютеров Одесского региона");
+        System.out.println("июл 2008 - апр 2011 (2 года 10 мес)");
+        System.out.println("Группа компанний ТОВ \"Гелиос\", ООО\"Гарт-с\" (http://www.home.gart-s.com.ua)");
+        System.out.println("от 100 до 250 сотрудников  Торговля розничная / Retail   (Компания более 15 лет на рынке Украины имеет около 60 ");
+        System.out.println("розничных магазинов в крупных городах Украины (Киев, Одесса, Донецк, Львов, Мариуполь) 'C7анимается ");
+        System.out.println("продажей товаров для дома (бытовая химия, косметика, парфюмерия, посуда)) ");
+        System.out.println("Обязанности:");
+        System.out.println("Маркетинг и анализ рынка компьтерного оборудования и расходных материалов к нему для последущей ");
+        System.out.println("закупки при открытие новых торговых точек и обслуживанию имеющихся");
+        System.out.println("Разработка , ведение бюджета(договора и оплата телефонии интернета и прочее) и оптимизация расходов ит ");
+        System.out.println("структуры региона");
+        System.out.println("Контроль за использованием расходных материалов ит структуры магазинов, офисов");
+        System.out.println("Контроль логистики (введение переговоров с постовщиком об условиях поставок нового оборудование или ");
+        System.out.println("замена устаревщего, скидках, условиях оплаты)Работа с 18 магазинами (13 Одесских, 5 Николаевских ");
+        System.out.println("дистанционно), и двумя офисами неподсредственная работа с касиирами администраторами управляющеми ");
+        System.out.println("(обучения, контроль, помощь) итого количество персонала от 57 человек");
+        System.out.println("Достижения:");
+        System.out.println("Бесперебойная, беспроблемная работа розничной сети, офисов региона");
+        System.out.println("Рекомендации");
+        System.out.println("готов предоставить по требованию");
+        System.out.println("Образование");
+        System.out.println("______________________________________________");
+        System.out.println("Политехнический университет (Одесса)");
+        System.out.println("Год окончания 2010 ");
+        System.out.println("инженер компьютерных систем и сетей специалист ");
+        System.out.println(" Владение языками");
+        System.out.println("____________________________________________________");
+        System.out.println("Английский - средний ");
+        System.out.println("Русский - родной ");
+        System.out.println("Могу проходить собеседование на этом языке");
+        System.out.println("Украинский - родной ");
+        System.out.println("Могу проходить собеседование на этом языке");
+        System.out.println("Курсы, тренинги, сертификаты");
+        System.out.println("____________________________________________________");
+        System.out.println("CCNA");
+        System.out.println("Год окончания 2012 ");
+        System.out.println("Закончил курсы ccna в Шаге ");
+        System.out.println("");
+        System.out.println("Персональной эффективности, Лидерская программа(Одесса)");
+        System.out.println("Год окончания 2010 ");
+        System.out.println("После прохождения тренинга стало проще общаться с торговым персоналом при ");
+        System.out.println("объяснение новых задач и работе с техникой");
+        System.out.println("Облегчилась организационная работа по закупки и обслуживанию техники");
+        System.out.println("");
+        System.out.println("Дополнительная информация");
+        System.out.println("____________________________________________________");
+        System.out.println("Водительское удостоверение");
+        System.out.println(" В,В1");
+        System.out.println("");
+        System.out.println("Компьютерные навыки");
+        System.out.println("Операционные системы - Windows (7,Xp,Me,98,95)администрирование");
+        System.out.println("Ubuntu, Freebsd,Windows server 2008 r2 -уверенный пользователь");
+        System.out.println("Программы удаленного доступа VNC, TeamViewer, Radmin");
+        System.out.println("Антивирусы - Nod32, Касперский");
+        System.out.println("Восстановление - norton ghost, acronis");
+        System.out.println("Офисные - openoffice, microsoft office, outlook, the bat, pop3, Amstop, Inventory");
+        System.out.println("");
+        System.out.println("Увлечения и интересы");
+        System.out.println(" Боулинг, Бильярд, Туристические походы ");
+        System.out.println("");
+        System.out.println("Личные качества");
+        System.out.println("Честность");
+        System.out.println("Работа на результат");
+        System.out.println("Пунктуальность");
+        System.out.println("Умение работать в команде");
+        System.out.println("Ответственность");
+        */       
+    }
+    
+}
